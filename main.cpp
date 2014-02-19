320f65038c9a174ddd3d70ffed6152ae8a32e98e
LoganJones2 <Logan.Jones@cameron.edu>
2014-02-19 14:02:52 -0600

    Added second machine

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 using namespace car_wash;
 
 int main(){
-	Queue<int> q;
+	Queue<int> q, q2;
 
 	int simulation_length, wash_time;
 	float r_rate;
@@ -19,17 +19,22 @@ int main(){
 	cout << "Enter arrival rate: ";
 	cin >> r_rate;
 
-	wash_machine myMachine(wash_time);
-	arrival myArrival(r_rate);
-	averager myAverager;
+	wash_machine myMachine(wash_time), myMachine2(wash_time);
+	arrival myArrival(r_rate), myArrival2(r_rate);
+	averager myAverager, myAverager2;
 	int car_denied = 0; 
 	
 	srand(time(NULL));
 
 	for (int i = 1; i <= simulation_length; i++){
 		if (myArrival.is_car_coming()){
-			if(!q.is_full()){
-				q.push(i);		//put car in queue
+			if(!q.is_full() && !q2.is_full()){
+				if (q.get_size() <= q2.get_size()){
+					q.push(i);		//put car in queue
+				}
+				else{
+					q2.push(i);
+				}
 			}
 			else{
 				car_denied++;
@@ -46,15 +51,34 @@ int main(){
 		}
 
 		myMachine.one_second();
+
+		if (!myMachine2.is_busy() && !q2.is_empty()){
+			//if available			if car is waiting
+
+			int next = q2.get_front();	//take a car from the front of the queue
+			q2.pop();
+			myAverager2.plus_next_number(i - next);	//subtracting time car was added from current time
+			myMachine2.start_washing();
+		}
+
+		myMachine2.one_second();
 	}
 
 	while (!q.is_empty()){
 		q.pop();
 		car_denied++;
 	}
-	cout << "Average waiting time: " << myAverager.average_time() << endl;
-	cout << "Total number of cars serviced: " << myAverager.how_many_cars() << endl;
-	cout << "Number of cars denied: " << car_denied << endl;
+
+	while (!q2.is_empty()){
+		q2.pop();
+		car_denied++;
+	}
+
+	cout << "Average waiting time for first machine: " << myAverager.average_time() << endl;
+	cout << "Total number of cars serviced by first machine: " << myAverager.how_many_cars() << endl;
+	cout << "Average waiting time for second machine: " << myAverager2.average_time() << endl;
+	cout << "Total number of cars serviced by second machine: " << myAverager2.how_many_cars() << endl;
+	cout << "Total number of cars denied: " << car_denied << endl;
 
 	return 0;
 }
