cf7517ff9a7da6c37eb1e7650a4bac3e3e792725
Dan Hill <dan@danhill.us>
2014-03-01 17:09:20 -0600

    added implimentation framework

--- car_wash.cpp
+++ car_wash.cpp
@@ -1,1 +1,46 @@
 #include "Car_Wash.h"
+
+Car_Wash::Car_Wash(int new_simulation_length, float new_arrival_rate)
+{
+
+}
+
+void Car_Wash::add_machine(int work_time)
+{
+
+}
+
+void Car_Wash::set_machine_wash_time(int index, int time)
+{
+
+}
+
+int Car_Wash::denied()
+{
+	return 0;
+}
+
+double Car_Wash::average_wait(int index)
+{
+	return 0;
+}
+
+double Car_Wash::average_wait()
+{
+	return 0;
+}
+
+int Car_Wash::serviced(int index)
+{
+	return 0;
+}
+
+int Car_Wash::serviced()
+{
+	return 0;
+}
+
+int Car_Wash::count()
+{
+	return 0;
+}
